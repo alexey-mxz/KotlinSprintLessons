da5ce0733215cc428fe1b5981d856064d19f12e0
Admin <Admin@>
2025-06-12 21:17:00 +0200

    KS-1-1 two-five words

---
+++ src/main/kotlin/KS-1-1.kt
@@ -0,0 +1,11 @@
+package org.example
+
+fun main() {
+
+    val numberOfOrders: Int = 75
+    val thanksForYourPurchase: String = "Thanks for your purchase"
+
+    println(numberOfOrders)
+    println(thanksForYourPurchase)
+
+}
